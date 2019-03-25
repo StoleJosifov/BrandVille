fd31f54f99d95c4ada27bfbecef4437a4f865b9c
Stole Josiov <stole.josifov@alphatheta.net>
2019-03-25 13:05:26 +0200

    Add project files.

---
+++ ManiaStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManiaStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
